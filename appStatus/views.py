d7cc48c42e7409a48e93a558f29bc126ecca0590
Stefanie Aiken <stefanieaiken@Stefanies-MacBook-Air.local>
2020-08-29 22:32:47 -0400

    updates to home & apply view, added applicant view

--- appStatus/views.py
+++ appStatus/views.py
@@ -1,12 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Applicant, Status
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'appStatus/dashboard.html')
+    status = Status.objects.all()
+    applicants = Applicant.objects.all()
+
+    total_applicants = applicants.count()
+
+    decision_pending = status.filter(status='Pending').count()
+    ready_review = status.filter(status='Accepted').count()
+
+    context = {'status': status, 'applicants': applicants, 'total_applicants': total_applicants,
+               'decision_pending': decision_pending, 'ready_review': ready_review}
+
+    return render(request, 'appStatus/dashboard.html', context)
+
+
+def applications(request):
+    application = Applicant.objects.all()
+
+    return render(request, 'appStatus/applications.html', {'applications': application})
 
 
 def apply(request):
+
     return render(request, 'appStatus/apply.html')
+
+
+def applicant(request, pk_test):
+    applicant = Applicant.objects.get(id=pk_test)
+
+    applicants = Applicant.objects.all()
+
+    context = {'applicant': applicant, 'applicants': applicants}
+    return render(request, 'appStatus/applicant.html', context)
