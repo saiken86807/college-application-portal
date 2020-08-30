ea48516c6ed7709577415bfa9f9813b013840f09
Stefanie Aiken <stefanieaiken@Stefanies-MacBook-Air.local>
2020-08-29 22:31:11 -0400

    added new ur paths

--- appStatus/urls.py
+++ appStatus/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('apply', views.apply),
+    path('', views.home, name="home"),
+    path('apply', views.apply, name="apply"),
+    path('applications', views.applications, name="applications"),
+    path('applicant/<str:pk_test>/', views.applicant, name="applicants"),
 ]
