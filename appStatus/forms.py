994db888e13ff75c85d6fadd14886bd5146c09f0
Stefanie Aiken <stefanieaiken@Stefanies-MacBook-Air.local>
2020-09-08 22:05:40 -0400

    updated forms & added createuserform

--- appStatus/forms.py
+++ appStatus/forms.py
@@ -1,14 +1,23 @@
 from django.forms import ModelForm
-from .models import Applicant, Status
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+from .models import Application
 
 
 class ApplicantForm(ModelForm):
     class Meta:
-        model = Applicant
+        model = Application
         fields = ['name', 'school', 'major', 'GPA']
 
 
 class DecisionForm(ModelForm):
     class Meta:
-        model = Status
-        fields = '__all__'
+        model = Application
+        fields = ['name', 'school', 'major', 'GPA', 'status']
+
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
