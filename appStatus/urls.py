81fd6b3c51c5ec47cdf3ef0a6a744f333091bf48
Stefanie Aiken <stefanieaiken@Stefanies-MacBook-Air.local>
2020-09-08 22:09:05 -0400

    added urls for register, login/out user

--- appStatus/urls.py
+++ appStatus/urls.py
@@ -2,10 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('register/', views.registerPage, name="register"),
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+
     path('', views.home, name="home"),
+    path('user', views.userPage, name='user-page'),
     path('apply', views.apply, name="apply"),
     path('applications', views.applications, name="applications"),
-    path('applicant/<str:pk_test>/', views.applicant, name="applicants"),
+    # path('applicant/<str:pk_test>/', views.applicant, name="applicants"),
     path('application_review/<str:pk>/',
          views.applicationReview, name='application_review')
 ]
