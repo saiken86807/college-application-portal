783dd75137db3225db0b30ef0514834fe65a9c94
Stefanie Aiken <stefanieaiken@Stefanies-MacBook-Air.local>
2020-09-16 13:48:17 -0400

    rmoved unused javascript

--- static/js/main.js
+++
@@ -1,7 +0,0 @@
-function disbaleButton(){
-	document.getElementById('submit').disabled = true;
-}
-function save() {
-	var msg = document.getElementById('msg');
-	msg.innerHTML = 'Your Application has been submitted';
-}
