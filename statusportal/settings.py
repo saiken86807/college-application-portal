9a0854d4b5e1291cf93399938b4f120082379803
Stefanie Aiken <stefanieaiken@Stefanies-MacBook-Air.local>
2020-09-16 14:00:24 -0400

    updated key

--- statusportal/settings.py
+++ statusportal/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('STATUSPORTAL_KEY')
+SECRET_KEY = 'k+bkpw(1tl)n77_(y4tp!jldfe8(7&+p*5--5rtp8ht+(1k$lv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
