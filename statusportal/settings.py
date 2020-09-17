c731bf0cb67690755427697263a47be1205c7117
Stefanie Aiken <stefanieaiken@Stefanies-MacBook-Air.local>
2020-09-16 20:42:47 -0400

    adding missing slash to media_url

--- statusportal/settings.py
+++ statusportal/settings.py
@@ -129,7 +129,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-MEDIA_URL = '/images'
+MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
