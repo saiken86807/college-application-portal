95f012021fa4cc4855c262de4139296a8de54a1e
Stefanie Aiken <stefanieaiken@Stefanies-MacBook-Air.local>
2020-09-20 18:31:33 -0400

    updates to settings.py file

--- statusportal/settings.py
+++ statusportal/settings.py
@@ -16,8 +16,7 @@ import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-#BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 
 # Quick-start development settings - unsuitable for production
